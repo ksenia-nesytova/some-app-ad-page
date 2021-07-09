d8aef308a199a4cd941e64ddc6fff2ee3575960f
ksenia-nesytova <alleheldentot@gmail.com>
2021-07-09 14:30:41 +0700

    menu btn changes on click

--- menu.js
+++ menu.js
@@ -2,4 +2,5 @@ const menuBtn = document.getElementById("menuBtn")
 
 const nav = document.getElementById("nav")
 
-menuBtn.addEventListener("click", () => nav.classList.toggle("visible"))
+menuBtn.addEventListener("click", () => { nav.classList.toggle("visible");
+menuBtn.textContent === "|||" ? menuBtn.textContent = "\u2716" : menuBtn.textContent = "|||"})
